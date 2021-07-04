01d375165b9dba858a34bb259d0a76f3c88fedf2
michal <michal32274@gmail.com>
2021-07-04 11:40:32 +0300

    end cection 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,10 +34,12 @@ namespace API
       services.AddDbContext<DataContext>(options =>
       options.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
       services.AddControllers();
+      services.AddCors();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
       });
+      
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +55,8 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
       app.UseAuthorization();
 
